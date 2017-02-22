124c5b1cde0614c1d6ea05dfcc333ace947fad05
Joe Melanson <joseph-melanson@hotmail.com>
2017-02-22 17:07:37 -0500

    initial css

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,70 +1,69 @@
 import React from 'react';
-
+var hostName = 'https://cors-anywhere.herokuapp.com/https://decode-bot-project-sql-ajdez.c9users.io';
 class MainComponent extends React.Component {
-var hostName = 'https://decode-bot-project-sql-ajdez.c9users.io'
-  fetchData1(){
-    fetch(`${hostName}/`)
-    .then(response => response.json())
-    then(x =>
-      this.state({
-
-      })
+  constructor() {
+    super()
+    this.state = {}
+  }
+  fetchDataSales() {
+    fetch(`${hostName}/sales`).then(response => console.log(response))
+    //response.json())
+      .then(x => {
+      console.log(x)
+      this.setState({x: x})
+    })
+  }
+  fetchDataExpenses() {
+    fetch(`${hostName}/expenses`).then(response => response.json()).then(x => {
+      this.setState({x: x})
+    })
+  }
+  fetchDataCompany() {
+    fetch(`${hostName}/Company`).then(response => response.json()).then(x => {
+      this.setState({x: x})
+    })
+  }
+  fetchData4() {
+    fetch(`${hostName}/`).then(response => response.json()).then(x => {
+      this.setState({x: x})
+    })
+  }
+  fetchData5() {
+    fetch(`${hostName}/`).then(response => response.json()).then(x => {
+      this.setState({x: x})
+    })
+  }
+  fetchData6() {
+    fetch(`${hostName}/`).then(response => response.json()).then(x => {
+      this.setState({x: x})
+    })
+  }
+  fetchData7() {
+    fetch(`${hostName}/`).then(response => response.json()).then(x => {
+      this.setState({x: x})
+    })
+  }
+  fetchData8() {
+    fetch(`${hostName}/`).then(response => response.json()).then(x => {
+      this.setState({x: x})
+    })
+  }
+  componentDidMount() {
+    this.fetchDataSales()
+  }
+  render() {
+    return (
+      <div className="main-component">
+        <div className="sales tile">${this.state.x}1</div>
+        <div className="expenses tile">2</div>
+        <div className="profits tile">3</div>
+        <div className="margin tile">4</div>
+        <div className="clients tile">5</div>
+        <div className="sales-expense-profit tile">6</div>
+        <div className="sales-table tile">7</div>
+        <div className="top-clients tile">8</div>
+      </div>
     )
   }
-  fetchData2(){
-    fetch(`${hostName}/`)
-    .then(response => response.json())
-    then(x =>
-      this.state({
-
-      })
-    )
-  }
-  fetchData3(){
-    fetch(`${hostName}/`)
-    .then(response => response.json())
-    then(x =>
-      this.state({
-
-      })
-    )
-  }
-  fetchData4(){
-    fetch(`${hostName}/`)
-    .then(response => response.json())
-    then(x =>
-      this.state({
-
-      })
-    )
-  }
-  fetchData5(){
-    fetch(`${hostName}/`)
-    .then(response => response.json())
-    then(x =>
-      this.state({
-
-      })
-    )
-  }
-
-
-
-render() {
-  return (
-    <div className="main-component">
-      <div className="sales"></div>
-      <div className="expenses"></div>
-      <div className="profits"></div>
-      <div className="margin"></div>
-      <div className="clients"></div>
-      <div className="sales-expense-profit"></div>
-      <div className="sales-table"></div>
-      <div className="top-clients"></div>
-    </div>
-  )
 }
-
-}
-
 export default MainComponent;
