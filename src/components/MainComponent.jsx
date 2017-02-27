3e9625f720c03e894fb406ba262f3414457df287
Nathaniel <nat.kitzke@gmail.com>
2017-02-27 15:53:39 -0500

    cleanup

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -7,8 +7,6 @@ import SalesExpenseProfit from './charts/SalesExpenseProfit';
 import Sales from './charts/Sales';
 import Expenses from './charts/Expenses';
 import SalesTable from "./charts/SalesTable";
-import GoalGauge from "./charts/goalGauge";
-import GoalsTable from "./charts/GoalsTable";
 import GoalCharts from "./charts/GoalCharts";
 import { Paper } from 'material-ui';
 
@@ -23,14 +21,14 @@ export default class extends React.Component {
         <Paper className="tile-xs"><Profits/></Paper>
         <Paper className="tile-xs"><AverageDealSize/></Paper>
         <Paper className="tile-xs"><Expenses/></Paper>
-        <Paper className="tile-sm sales"><SalesExpenseProfit/></Paper>
-        <Paper className="tile-sm client"><TopClients/></Paper>
-
-        <Paper className="tile-sm-long"><SalesTable /></Paper>
+        <Paper className="tile-sm"><SalesExpenseProfit/></Paper>
+        <Paper className="tile-sm"><TopClients/></Paper>
 
         <GoalCharts />
 
-        <Paper className="tile goalTable"><GoalsTable/></Paper>
+        <Paper className="tile-sm-long"><SalesTable /></Paper>
+        
+
       </div>
     )
   }
