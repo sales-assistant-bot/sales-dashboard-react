b15038c1287d398ec7bcbf13ad16e52ae40d266c
Nathaniel <nat.kitzke@gmail.com>
2017-02-28 18:05:02 -0500

    goalds

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import MainComponent from './components/MainComponent';
 import App from './App';
 import './index.css';
+
 
 
 // Needed for onTouchTap
