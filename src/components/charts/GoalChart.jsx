260b7edfa9a05338ee00f36952b866db14c21215
Ziad Saab <ziad.saab@gmail.com>
2017-03-02 10:51:15 -0500

    reverted progress bars to donut chart for goals

--- src/components/charts/GoalChart.jsx
+++ src/components/charts/GoalChart.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-// import { Doughnut } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 import { Paper } from 'material-ui';
 import moment from 'moment'
-import { Progress } from 'reactstrap';
-
 
 import Loading from './Loading';
 import './goalCharts.css'
@@ -12,53 +10,48 @@ import './goalCharts.css'
 export default class extends Component {
 
   render() {
-    const { data } = this.props;
-    let mainValue = data.CurrentAmount/data.GoalAmount * 100;
-    let overValue;
-    if (mainValue > 100) {
-      overValue = mainValue-100;
-      mainValue = 100;
+    const options = {
+      legend: {
+        display: false
+      },
+      maintainAspectRatio: true,
+      responsive: true
     }
     return (
-      <Paper className="goal">
-        <div className="goal-amount chart-title">
-          {` Goal: $${data.GoalAmount.toMoney()}`}
-        </div>
-        {/* <div className="goal-time-range">
-          {
-            moment(data.StartDate).format('MMM D') +
-                " - " +
-            moment(data.EndDate).format('MMM D') + " "
-          }
-        </div> */}
-        <div className="goal-time-remaining">
-          {moment(data.EndDate).fromNow(true)}
+      <Paper>
+        <div className="chart-title">
+          <span className="goal-date-range">
+            {
+              moment(this.props.data.StartDate).format('MMM Do') +
+                  " - " +
+              moment(this.props.data.EndDate).format('MMM Do') + " "
+            }
+          </span>
+          {" : "}
+          {moment(this.props.data.EndDate).fromNow(true)}
           {" remaining."}
         </div>
-
-        <div className="progress-bars">
-          <Progress animated bar color={overValue? "success" : "warning"} value={mainValue}>
-            {overValue ? <span className="goal-text-congrats">{`Congratulations, goal reached!`}</span> : null}
-          </Progress>
-          {overValue? <Progress animated bar color="info" value={overValue}/> : null}
-        </div>
-        {overValue? <span className="goal-text">{`You've gone $${Math.round(Number(data.CurrentAmount-data.GoalAmount)).toMoney()} past your goal!` }</span> : null}
-
-        {!overValue? <span className="goal-text">{`You've sold $${Math.round(data.CurrentAmount).toMoney()} so far.`}</span> : null}
-
+        <Doughnut data={this.shapeData(this.props.data)} options={options} height={100}/>
       </Paper>
     )
   }
-}
 
-
-Number.prototype.toMoney = function(decimals, decimal_sep, thousands_sep) {
-   var n = this,
-   c = isNaN(decimals) ? 2 : Math.abs(decimals),
-   d = decimal_sep || '.',
-   t = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-   sign = (n < 0) ? '-' : '',
-   i = parseInt(n = Math.abs(n).toFixed(c)) + '',
-   j = ((j = i.length) > 3) ? j % 3 : 0;
-   return sign + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : '');
+  shapeData = (goal) => {
+    return {
+      labels: [
+        'Achieved', 'Goal Remaining',
+      ],
+      datasets: [
+        {
+          data: [
+            goal.CurrentAmount, goal.AmountMissing
+          ],
+          backgroundColor: [
+            '#BDC39D', '#FCCC8E'
+          ],
+          hoverBackgroundColor: ['#BDC39D', '#FCCC8E']
+        }
+      ]
+    }
+  }
 }
